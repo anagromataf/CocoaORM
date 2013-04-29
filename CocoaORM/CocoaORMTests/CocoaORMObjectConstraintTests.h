4ed03df8ba3a35fc526546b3b429bc6cec414bbd
Tobias Kräntzer <info@tobias-kraentzer.de>
2013-04-29 14:34:48 +0200

    Add constraint test for objects

---
+++ CocoaORM/CocoaORMTests/CocoaORMObjectConstraintTests.h
@@ -0,0 +1,13 @@
+//
+//  CocoaORMObjectConstraintTests.h
+//  CocoaORM
+//
+//  Created by Tobias Kräntzer on 29.04.13.
+//  Copyright (c) 2013 Tobias Kräntzer. All rights reserved.
+//
+
+#import "CocoaORMTestCase.h"
+
+@interface CocoaORMObjectConstraintTests : CocoaORMTestCase
+
+@end
